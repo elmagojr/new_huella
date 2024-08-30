10cfa2dc96cd024df4753f4d97a13c4703604ae4
Johann Flores <johann.flores@red-tecnologica.com>
2024-08-30 17:02:26 -0600

    Nueva esquem ahuellas

---
+++ DDigital/DDigital/Main_Menu.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace DDigital
+{
+    partial class Main_Menu
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main_Menu));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.radio_I5 = new System.Windows.Forms.RadioButton();
+            this.radio_I4 = new System.Windows.Forms.RadioButton();
+            this.radio_I3 = new System.Windows.Forms.RadioButton();
+            this.radio_I2 = new System.Windows.Forms.RadioButton();
+            this.radio_I1 = new System.Windows.Forms.RadioButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.radio_D1 = new System.Windows.Forms.RadioButton();
+            this.radio_D5 = new System.Windows.Forms.RadioButton();
+            this.radio_D4 = new System.Windows.Forms.RadioButton();
+            this.radio_D3 = new System.Windows.Forms.RadioButton();
+            this.radio_D2 = new System.Windows.Forms.RadioButton();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pic_huella = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_huella)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(301, 34);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(474, 479);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.radio_I5);
+            this.tabPage1.Controls.Add(this.radio_I4);
+            this.tabPage1.Controls.Add(this.radio_I3);
+            this.tabPage1.Controls.Add(this.radio_I2);
+            this.tabPage1.Controls.Add(this.radio_I1);
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(466, 450);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // radio_I5
+            // 
+            this.radio_I5.CheckAlign = System.Drawing.ContentAlignment.TopRight;
+            this.radio_I5.Location = new System.Drawing.Point(338, 269);
+            this.radio_I5.Name = "radio_I5";
+            this.radio_I5.Size = new System.Drawing.Size(24, 23);
+            this.radio_I5.TabIndex = 7;
+            this.radio_I5.TabStop = true;
+            this.radio_I5.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.radio_I5.UseVisualStyleBackColor = true;
+            // 
+            // radio_I4
+            // 
+            this.radio_I4.CheckAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.radio_I4.Location = new System.Drawing.Point(274, 144);
+            this.radio_I4.Name = "radio_I4";
+            this.radio_I4.Size = new System.Drawing.Size(22, 115);
+            this.radio_I4.TabIndex = 6;
+            this.radio_I4.TabStop = true;
+            this.radio_I4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.radio_I4.UseVisualStyleBackColor = true;
+            // 
+            // radio_I3
+            // 
+            this.radio_I3.CheckAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.radio_I3.Location = new System.Drawing.Point(221, 120);
+            this.radio_I3.Name = "radio_I3";
+            this.radio_I3.Size = new System.Drawing.Size(26, 117);
+            this.radio_I3.TabIndex = 5;
+            this.radio_I3.TabStop = true;
+            this.radio_I3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.radio_I3.UseVisualStyleBackColor = true;
+            // 
+            // radio_I2
+            // 
+            this.radio_I2.CheckAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.radio_I2.Location = new System.Drawing.Point(169, 139);
+            this.radio_I2.Name = "radio_I2";
+            this.radio_I2.Size = new System.Drawing.Size(27, 115);
+            this.radio_I2.TabIndex = 4;
+            this.radio_I2.TabStop = true;
+            this.radio_I2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.radio_I2.UseVisualStyleBackColor = true;
+            // 
+            // radio_I1
+            // 
+            this.radio_I1.CheckAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.radio_I1.Location = new System.Drawing.Point(124, 178);
+            this.radio_I1.Name = "radio_I1";
+            this.radio_I1.Size = new System.Drawing.Size(20, 81);
+            this.radio_I1.TabIndex = 1;
+            this.radio_I1.TabStop = true;
+            this.radio_I1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.radio_I1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(90, 95);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(303, 328);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.radio_D1);
+            this.tabPage2.Controls.Add(this.radio_D5);
+            this.tabPage2.Controls.Add(this.radio_D4);
+            this.tabPage2.Controls.Add(this.radio_D3);
+            this.tabPage2.Controls.Add(this.radio_D2);
+            this.tabPage2.Controls.Add(this.pictureBox3);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(466, 450);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // radio_D1
+            // 
+            this.radio_D1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.radio_D1.CheckAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.radio_D1.FlatAppearance.BorderColor = System.Drawing.SystemColors.ScrollBar;
+            this.radio_D1.FlatAppearance.BorderSize = 10;
+            this.radio_D1.FlatAppearance.CheckedBackColor = System.Drawing.Color.Red;
+            this.radio_D1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.radio_D1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Cyan;
+            this.radio_D1.ForeColor = System.Drawing.Color.Transparent;
+            this.radio_D1.Location = new System.Drawing.Point(111, 268);
+            this.radio_D1.Name = "radio_D1";
+            this.radio_D1.Size = new System.Drawing.Size(26, 23);
+            this.radio_D1.TabIndex = 1;
+            this.radio_D1.TabStop = true;
+            this.radio_D1.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.radio_D1.UseVisualStyleBackColor = false;
+            // 
+            // radio_D5
+            // 
+            this.radio_D5.CheckAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.radio_D5.Location = new System.Drawing.Point(326, 174);
+            this.radio_D5.Name = "radio_D5";
+            this.radio_D5.Size = new System.Drawing.Size(20, 74);
+            this.radio_D5.TabIndex = 6;
+            this.radio_D5.TabStop = true;
+            this.radio_D5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.radio_D5.UseVisualStyleBackColor = true;
+            // 
+            // radio_D4
+            // 
+            this.radio_D4.CheckAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.radio_D4.Location = new System.Drawing.Point(274, 144);
+            this.radio_D4.Name = "radio_D4";
+            this.radio_D4.Size = new System.Drawing.Size(27, 104);
+            this.radio_D4.TabIndex = 4;
+            this.radio_D4.TabStop = true;
+            this.radio_D4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.radio_D4.UseVisualStyleBackColor = true;
+            // 
+            // radio_D3
+            // 
+            this.radio_D3.CheckAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.radio_D3.Location = new System.Drawing.Point(222, 121);
+            this.radio_D3.Name = "radio_D3";
+            this.radio_D3.Size = new System.Drawing.Size(30, 127);
+            this.radio_D3.TabIndex = 3;
+            this.radio_D3.TabStop = true;
+            this.radio_D3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.radio_D3.UseVisualStyleBackColor = true;
+            // 
+            // radio_D2
+            // 
+            this.radio_D2.CheckAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.radio_D2.Location = new System.Drawing.Point(170, 144);
+            this.radio_D2.Name = "radio_D2";
+            this.radio_D2.Size = new System.Drawing.Size(33, 104);
+            this.radio_D2.TabIndex = 2;
+            this.radio_D2.TabStop = true;
+            this.radio_D2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.radio_D2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(79, 95);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(302, 321);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 0;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pic_huella
+            // 
+            this.pic_huella.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pic_huella.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic_huella.Location = new System.Drawing.Point(12, 62);
+            this.pic_huella.Name = "pic_huella";
+            this.pic_huella.Size = new System.Drawing.Size(216, 196);
+            this.pic_huella.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_huella.TabIndex = 1;
+            this.pic_huella.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 265);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // Main_Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1426, 646);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.pic_huella);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main_Menu";
+            this.Text = "DDigital";
+            this.Load += new System.EventHandler(this.Main_Menu_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_huella)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.RadioButton radio_D4;
+        private System.Windows.Forms.RadioButton radio_D3;
+        private System.Windows.Forms.RadioButton radio_D2;
+        private System.Windows.Forms.RadioButton radio_D1;
+        private System.Windows.Forms.RadioButton radio_I5;
+        private System.Windows.Forms.RadioButton radio_I4;
+        private System.Windows.Forms.RadioButton radio_I3;
+        private System.Windows.Forms.RadioButton radio_I2;
+        private System.Windows.Forms.RadioButton radio_I1;
+        private System.Windows.Forms.RadioButton radio_D5;
+        private System.Windows.Forms.PictureBox pic_huella;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DDigital/DDigital/Main_Menu.cs
@@ -0,0 +1,338 @@
+﻿using DDigital.Utilidades;
+using DPUruNet;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DDigital
+{
+    public partial class Main_Menu : Form
+    {
+        private Reader _reader;
+        List<Fmd> preenrollmentFmds;
+        int count;
+
+        public bool InitializeReader()
+        {
+            _reader = ReaderCollection.GetReaders()[0]; // Obtén el primer lector disponible
+            if (_reader!= null)
+            {
+                Constants.ResultCode resultCode = _reader.Open(Constants.CapturePriority.DP_PRIORITY_COOPERATIVE);
+
+                if (resultCode != Constants.ResultCode.DP_SUCCESS)
+                {
+                    MessageBox.Show("No hay lector conectado");
+                    return false;
+                }
+
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("No hay lector conectado");
+                return false;
+            }
+           
+        }
+
+        public bool Reset
+        {
+            get { return reset; }
+            set { reset = value; }
+        }
+        private bool reset;
+
+
+        public void GetStatus()
+        {
+            using (Tracer tracer = new Tracer("Main_Menu::GetStatus"))
+            {
+                Constants.ResultCode result = _reader.GetStatus();
+
+                if ((result != Constants.ResultCode.DP_SUCCESS))
+                {
+                    reset = true;
+                    throw new Exception("" + result);
+                }
+
+                if ((_reader.Status.Status == Constants.ReaderStatuses.DP_STATUS_BUSY))
+                {
+                    Thread.Sleep(50);
+                }
+                else if ((_reader.Status.Status == Constants.ReaderStatuses.DP_STATUS_NEED_CALIBRATION))
+                {
+                    _reader.Calibrate();
+                }
+                else if ((_reader.Status.Status != Constants.ReaderStatuses.DP_STATUS_READY))
+                {
+                    throw new Exception("Reader Status - " + _reader.Status.Status);
+                }
+            }
+        }
+        public bool OpenReader()
+        {
+            // _reader = new Reader();
+            _reader = ReaderCollection.GetReaders()[0];
+
+            using (Tracer tracer = new Tracer("Main_Menu::OpenReader"))
+            {
+                reset = false;
+                Constants.ResultCode result = Constants.ResultCode.DP_DEVICE_FAILURE;
+
+                // Open reader
+                result = _reader.Open(Constants.CapturePriority.DP_PRIORITY_COOPERATIVE);
+          
+                if (result != Constants.ResultCode.DP_SUCCESS)
+                {
+                    MessageBox.Show("Error:  Lector no conectado" + result);
+                    reset = true;
+                    return false;
+                }
+
+                return true;
+            }
+        }
+
+        public bool StartCaptureAsync(Reader.CaptureCallback OnCaptured)
+        {
+            using (Tracer tracer = new Tracer("Main_Menu::StartCaptureAsync"))
+            {
+                // Activate capture handler
+                _reader.On_Captured += new Reader.CaptureCallback(OnCaptured);
+
+                // Call capture
+                if (!CaptureFingerAsync())
+                {
+                    return false;
+                }
+
+                return true;
+            }
+        }
+
+        public void CancelCaptureAndCloseReader(Reader.CaptureCallback OnCaptured)
+        {
+            using (Tracer tracer = new Tracer("Main_Menu::CancelCaptureAndCloseReader"))
+            {
+                if (_reader != null)
+                {
+                    _reader.CancelCapture();
+
+                    // Dispose of reader handle and unhook reader events.
+                    _reader.Dispose();
+
+                    if (reset)
+                    {
+                        _reader = null;
+                    }
+                }
+            }
+        }
+        public bool CaptureFingerAsync()
+        {
+            using (Tracer tracer = new Tracer("Main_Menu::CaptureFingerAsync"))
+            {
+                try
+                {
+                    GetStatus();
+
+                    Constants.ResultCode captureResult = _reader.CaptureAsync(Constants.Formats.Fid.ANSI, Constants.CaptureProcessing.DP_IMG_PROC_DEFAULT, _reader.Capabilities.Resolutions[0]);
+                    if (captureResult != Constants.ResultCode.DP_SUCCESS)
+                    {
+                        reset = true;
+                        throw new Exception("" + captureResult);
+                    }
+
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error:  " + ex.Message);
+                    return false;
+                }
+            }
+        }
+        public bool CheckCaptureResult(CaptureResult captureResult)
+        {
+            using (Tracer tracer = new Tracer("Main_Menu::CheckCaptureResult"))
+            {
+                if (captureResult.Data == null || captureResult.ResultCode != Constants.ResultCode.DP_SUCCESS)
+                {
+                    if (captureResult.ResultCode != Constants.ResultCode.DP_SUCCESS)
+                    {
+                        reset = true;
+                        throw new Exception(captureResult.ResultCode.ToString());
+                    }
+
+                    // Send message if quality shows fake finger
+                    if ((captureResult.Quality != Constants.CaptureQuality.DP_QUALITY_CANCELED))
+                    {
+                        throw new Exception("Calidad de fig - " + captureResult.Quality);
+                    }
+                    return false;
+                }
+
+                return true;
+            }
+        }
+
+        //public void OnCaptured(CaptureResult captureResult)
+        //{
+        //    if (this.InvokeRequired)
+        //    {
+        //        this.Invoke(new Action(() =>
+        //        {
+        //            OnCaptured(captureResult);
+        //        })); 
+        //        return;
+        //    }
+        //    UTILIDADES UT = new UTILIDADES();
+        //    try
+        //    {
+        //        // Check capture quality and throw an error if bad.
+        //        if (!this.CheckCaptureResult(captureResult)) return;
+
+        //        // Create bitmap
+        //        foreach (Fid.Fiv fiv in captureResult.Data.Views)
+        //        {
+        //            pic_huella.Image = UT.CreateBitmap(fiv.RawImage, fiv.Width, fiv.Height);
+        //            pic_huella.Refresh();   
+
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+
+        //    }
+        //}
+
+        public void OnCaptured(CaptureResult captureResult)
+        {
+
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Action(() =>
+                {
+                    OnCaptured(captureResult);
+                }));
+                return;
+            }
+            UTILIDADES UT = new UTILIDADES();
+            try
+                {
+                // Check capture quality and throw an error if bad.
+                if (!this.CheckCaptureResult(captureResult)) return;
+
+                count++;
+                label1.Text = "Escaneado " + count + " de 4 muestras";
+                DataResult<Fmd> resultConversion = FeatureExtraction.CreateFmdFromFid(captureResult.Data, Constants.Formats.Fmd.ANSI);
+
+                
+               // MessageBox.Show("A finger was captured "+(count));
+               // SendMessage(Action.SendMessage, "A finger was captured.  \r\nCount:  " + (count));
+
+                if (resultConversion.ResultCode != Constants.ResultCode.DP_SUCCESS)
+                {
+                    this.Reset = true;
+                    throw new Exception(resultConversion.ResultCode.ToString());
+                }
+
+                preenrollmentFmds.Add(resultConversion.Data);
+                
+                
+                if (count == 4)
+                {
+                    label1.Text = "Escaneado " + count + " de 4 muestras";
+                    DataResult<Fmd> resultEnrollment = DPUruNet.Enrollment.CreateEnrollmentFmd(Constants.Formats.Fmd.ANSI, preenrollmentFmds);
+
+                    if (resultEnrollment.ResultCode == Constants.ResultCode.DP_SUCCESS)
+                    {
+                        MessageBox.Show("An enrollment FMD was successfully created");
+                        // SendMessage(Action.SendMessage, "An enrollment FMD was successfully created.");
+                        // SendMessage(Action.SendMessage, "Place a finger on the reader.");
+                        preenrollmentFmds.Clear();
+                        count = 0;
+                        label1.Text = "Escaneado " + count + " de 4 muestras";
+                        return;
+                    }
+                    else if (resultEnrollment.ResultCode == Constants.ResultCode.DP_ENROLLMENT_INVALID_SET)
+                    {
+                        // SendMessage(Action.SendMessage, "Enrollment was unsuccessful.  Please try again.");
+                        // SendMessage(Action.SendMessage, "Place a finger on the reader.");
+                        MessageBox.Show("Una");
+                        preenrollmentFmds.Clear();
+                        count = 0;
+                        label1.Text = "Escaneado " + count + " de 4 muestras";
+                        return;
+                    }
+                    else if (resultEnrollment.ResultCode == Constants.ResultCode.DP_ENROLLMENT_NOT_READY)
+                    {
+                        label1.Text = "Muestras correctas son insuficientes, intente de nuevo";
+                        count = 0;
+                        return;
+                    }
+
+                }
+            
+
+                // SendMessage(Action.SendMessage, "Now place the same finger on the reader.");
+            }
+            catch (Exception ex)
+            {
+                // Send error message, then close form
+               // SendMessage(Action.SendMessage, "Error:  " + ex.Message);
+            }
+        }
+
+
+
+
+        public Main_Menu()
+        {
+            InitializeComponent();
+           
+            
+            
+         
+
+        }
+
+        private void Main_Menu_Load(object sender, EventArgs e)
+        {
+           bool lector = OpenReader();
+     
+            pic_huella.Image=null;
+            preenrollmentFmds = new List<Fmd>();
+            count = 0;
+            label1.Text = "Escaneado "+count+" de 4 muestras";
+            if (lector)
+            {
+                if (!this.StartCaptureAsync(this.OnCaptured))
+                {
+                    this.Close();
+                }
+            }
+
+
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
